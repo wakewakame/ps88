7fc856448522b68eee6ac7dfdb5040e28a9b6afc
wakewakame <0214.taiki@gmail.com>
2025-01-13 05:00:37 +0900

    フォントは submodule ではなく直接含めてしまうようにする

---
+++ crates/assets/roboto-mono/README.md
@@ -0,0 +1,4 @@
+# RobotoMono
+
+これは [RobotoMono](https://github.com/googlefonts/RobotoMono/tree/3479a228ba99f69d6e504e7d798a3f8e8239bbe7) を Rust の crate としてラップしたディレクトリです。
+フォントのライセンスを cargo-about で管理できるようにするため、このような形にしています。
--- crates/assets/roboto-mono/src/lib.rs
+++ crates/assets/roboto-mono/src/lib.rs
@@ -1,2 +1,2 @@
-pub const ROBOTO_MONO: &[u8] =
-    include_bytes!("../assets/RobotoMono/fonts/variable/RobotoMono[wght].ttf");
+pub const ROBOTO_MONO: &[u8] = include_bytes!("../assets/RobotoMono[wght].ttf");
+pub const ROBOTO_MONO_ITALIC: &[u8] = include_bytes!("../assets/RobotoMono-Italic[wght].ttf");
