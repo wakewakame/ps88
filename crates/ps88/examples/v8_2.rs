ce073297a7553c833b96538a87a5184f7a12210e
wakewakame <0214.taiki@gmail.com>
2025-08-12 01:53:00 +0900

    リファクタリング

--- crates/ps88/examples/v8_2.rs
+++ crates/ps88/examples/v8_2.rs
@@ -4,24 +4,24 @@ use std::collections::HashMap;
 use std::rc::Rc;
 use std::sync::Once;
 
-pub struct JsRuntime<Status> {
+struct JsRuntime<Status> {
     // isolate は status を参照しているため、drop される順番は isolate -> status とする必要がある。
     // そのため、フィールドの宣言順は isolate -> status とする。
     isolate: v8::OwnedIsolate,
-    status: Rc<RefCell<Status>>,
+    status: Status,
     callbacks: HashMap<String, v8::FunctionCallback>,
 }
 
 struct JsRuntimeContext(v8::Global<v8::Context>);
 
-pub struct CallbackInfo<'a, 'b> {
+struct CallbackInfo<'a, 'b> {
     scope: &'a mut v8::HandleScope<'b>,
     args: v8::FunctionCallbackArguments<'a>,
     rv: v8::ReturnValue<'a>,
 }
 
 impl<Status> JsRuntime<Status> {
-    pub fn new(status: Rc<RefCell<Status>>) -> Self {
+    pub fn new(status: Status) -> Self {
         static PUPPY_INIT: Once = Once::new();
         PUPPY_INIT.call_once(move || {
             let platform = v8::new_default_platform(0, false).make_shared();
@@ -37,11 +37,7 @@ impl<Status> JsRuntime<Status> {
         }
     }
 
-    fn reset(&mut self) {
-        self.isolate.remove_slot::<JsRuntimeContext>();
-    }
-
-    fn add_func<F: Fn(&mut Status, CallbackInfo) + Sized>(&mut self, name: &str, _: F) {
+    fn add_func<F: Fn(&Status, CallbackInfo) + Sized>(&mut self, name: &str, _: F) {
         const {
             assert!(
                 size_of::<F>() == 0,
@@ -49,7 +45,7 @@ impl<Status> JsRuntime<Status> {
             )
         }
         // Rust の関数やクロージャを v8::FunctionCallback としてラップする
-        unsafe extern "C" fn f<Status, F: Fn(&mut Status, CallbackInfo) + Sized>(
+        unsafe extern "C" fn f<Status, F: Fn(&Status, CallbackInfo) + Sized>(
             info: *const v8::FunctionCallbackInfo,
         ) {
             let info = unsafe { &*info };
@@ -57,16 +53,17 @@ impl<Status> JsRuntime<Status> {
             let args = v8::FunctionCallbackArguments::from_function_callback_info(info);
             let rv = v8::ReturnValue::from_function_callback_info(info);
             let info = CallbackInfo { scope, args, rv };
-            let status = info.args.data().cast::<v8::External>();
-            let status = unsafe { &*status.value().cast::<RefCell<Status>>() };
+            let status = info.args.data().try_cast::<v8::External>().unwrap();
+            let status = status.value().cast::<Status>();
+            let status = unsafe { &*status };
             let f = unsafe { std::mem::zeroed::<F>() };
-            f(&mut *status.borrow_mut(), info);
+            f(status, info);
         }
         self.callbacks.insert(name.to_string(), f::<Status, F>);
     }
 
     fn compile(&mut self, code: &str) {
-        self.reset();
+        self.isolate.remove_slot::<JsRuntimeContext>();
         let context = {
             let handle_scope = &mut v8::HandleScope::new(&mut self.isolate);
             let context = v8::Context::new(handle_scope, v8::ContextOptions::default());
@@ -76,8 +73,10 @@ impl<Status> JsRuntime<Status> {
             let scope = &mut v8::HandleScope::with_context(&mut self.isolate, &context);
             let context = v8::Local::new(scope, &context);
             let obj_t = v8::ObjectTemplate::new(scope);
-            let status =
-                v8::External::new(scope, Rc::as_ptr(&self.status) as *mut std::ffi::c_void);
+            let status = v8::External::new(
+                scope,
+                &mut self.status as *mut Status as *mut std::ffi::c_void,
+            );
             for (name, func) in self.callbacks.iter() {
                 let name = v8::String::new(scope, name).unwrap();
                 let func = v8::FunctionBuilder::<v8::FunctionTemplate>::new_raw(*func)
@@ -108,14 +107,14 @@ impl<Status> JsRuntime<Status> {
 }
 
 struct MyStatus {
-    audio: Option<v8::Global<v8::Function>>,
+    audio: Rc<RefCell<Option<v8::Global<v8::Function>>>>,
 }
 
 impl MyStatus {
-    fn audio(&mut self, mut info: CallbackInfo) {
-        let callback = info.args.get(0).cast::<v8::Function>();
+    fn audio(&self, mut info: CallbackInfo) {
+        let callback = info.args.get(0).try_cast::<v8::Function>().unwrap();
         let callback = v8::Global::new(info.scope, callback);
-        self.audio = Some(callback);
+        *self.audio.borrow_mut() = Some(callback);
         info.rv
             .set(v8::String::new(info.scope, "ok").unwrap().into());
     }
@@ -128,21 +127,26 @@ impl Drop for MyStatus {
 }
 
 fn main() {
-    let data = Rc::new(RefCell::new(MyStatus { audio: None }));
-    let mut app = JsRuntime::new(data.clone());
+    let audio = Rc::new(RefCell::new(None));
+    let data = MyStatus {
+        audio: audio.clone(),
+    };
+    let mut app = JsRuntime::new(data);
     app.add_func("audio", MyStatus::audio);
     app.compile("ps88.audio((n) => (n + 100))");
 
     {
-        let data = data.borrow_mut();
         let context = app.context();
         let scope = &mut v8::HandleScope::with_context(&mut app.isolate, &context);
-        let callback = data.audio.as_ref().unwrap();
-        let callback = v8::Local::new(scope, callback);
+        let audio = audio.borrow_mut();
+        let callback = v8::Local::new(scope, audio.as_ref().unwrap());
         let this = v8::undefined(scope).into();
         let arg = v8::Number::new(scope, 42f64).into();
         let result = callback.call(scope, this, &[arg]).unwrap();
-        println!("Callback result: {:?}", result.cast::<v8::Number>().value());
+        println!(
+            "Callback result: {:?}",
+            result.try_cast::<v8::Number>().unwrap().value()
+        );
     }
     drop(app);
 }
