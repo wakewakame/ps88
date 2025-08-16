99fba18dfc3ebc207c9867c838d0084321a32cc3
wakewakame <0214.taiki@gmail.com>
2025-08-17 06:26:27 +0900

    console.log への対応

--- crates/ps88/examples/v8_2.rs
+++ crates/ps88/examples/v8_2.rs
@@ -2,7 +2,6 @@
 TODO
 - unwrap を使わないようにする
 - テストを書く
-- console.log を使えるようにする
 */
 
 use deno_core::v8;
@@ -13,7 +12,8 @@ use std::sync::Once;
 
 // JavaScript の実行環境
 struct JsRuntime<Status> {
-    // NOTE: context は isolate に紐づくので、context は isolate より先に drop される必要がある
+    // NOTE: _inspector と context は isolate に紐づくので、これらは isolate より先に drop される必要がある
+    _inspector: Option<Inspector>,
     context: v8::Global<v8::Context>,
 
     // NOTE: isolate は間接的に status の生ポインタを参照しているため、isolate は status より先に drop される必要がある
@@ -38,6 +38,7 @@ impl<Status> JsRuntime<Status> {
             v8::Global::new(handle_scope, context)
         };
         Self {
+            _inspector: None,
             context,
             isolate,
             status,
@@ -45,13 +46,29 @@ impl<Status> JsRuntime<Status> {
     }
 
     // JavaScript の実行環境をリセット
+    // set_logger() や add_api() の設定もリセットされる。
     fn reset(&mut self) {
-        //self.isolate
-        //    .memory_pressure_notification(v8::MemoryPressureLevel::Critical);
+        self._inspector = None;
         self.context = {
             let handle_scope = &mut v8::HandleScope::new(&mut self.isolate);
             let context = v8::Context::new(handle_scope, v8::ContextOptions::default());
             v8::Global::new(handle_scope, context)
+        };
+    }
+
+    // console.log() の出力を得るためのロガーを設定
+    fn set_logger<F: Fn(String) + 'static>(&mut self, logger: F) {
+        // NOTE:
+        // 新しい inspector を作った後に古い inspector を drop すると
+        // 古い inspector のデストラクタが新しい inspector に影響して
+        // console.log の出力を得られなくなってしまう。
+        // そのため、先にここで古いインスタンスを drop しておく。
+        self._inspector = None;
+        self._inspector = {
+            let scope = &mut v8::HandleScope::with_context(&mut self.isolate, &self.context);
+            let context = v8::Local::new(scope, &self.context);
+            let inspector = Inspector::new(scope, context, logger);
+            Some(inspector)
         };
     }
 
@@ -164,6 +181,75 @@ struct CallbackInfo<'a, 'b> {
     rv: v8::ReturnValue<'a>,
 }
 
+struct Inspector {
+    // NOTE: _inspector は _client の参照を抱えるため client より先に drop される必要がある
+    _inspector: v8::UniqueRef<v8::inspector::V8Inspector>,
+    _client: Box<InspectorClient>,
+}
+
+impl Inspector {
+    fn new<F: Fn(String) + 'static>(
+        scope: &mut v8::HandleScope,
+        context: v8::Local<v8::Context>,
+        logger: F,
+    ) -> Self {
+        let mut client = {
+            let base = v8::inspector::V8InspectorClientBase::new::<InspectorClient>();
+            let logger = Box::new(logger);
+            Box::new(InspectorClient { base, logger })
+        };
+        let inspector = {
+            let mut inspector = v8::inspector::V8Inspector::create(scope, &mut *client);
+            let context_name = v8::inspector::StringView::from(&b"main realm"[..]);
+            let aux_data = v8::inspector::StringView::from(r#"{"isDefault": true}"#.as_bytes());
+            inspector.context_created(context, 1, context_name, aux_data);
+            inspector
+        };
+        Self {
+            _inspector: inspector,
+            _client: client,
+        }
+    }
+}
+
+struct InspectorClient {
+    base: v8::inspector::V8InspectorClientBase,
+    logger: Box<dyn Fn(String)>,
+}
+
+// 参考: https://github.com/denoland/deno_core/blob/75759fb5127982bdaf71e68f04dee01531d6591b/core/inspector.rs#L119
+impl v8::inspector::V8InspectorClientImpl for InspectorClient {
+    fn base(&self) -> &v8::inspector::V8InspectorClientBase {
+        &self.base
+    }
+
+    fn base_mut(&mut self) -> &mut v8::inspector::V8InspectorClientBase {
+        &mut self.base
+    }
+
+    unsafe fn base_ptr(this: *const Self) -> *const v8::inspector::V8InspectorClientBase
+    where
+        Self: Sized,
+    {
+        // SAFETY: this pointer is valid for the whole lifetime of inspector
+        unsafe { std::ptr::addr_of!((*this).base) }
+    }
+
+    fn console_api_message(
+        &mut self,
+        _context_group_id: i32,
+        _level: i32,
+        message: &v8::inspector::StringView,
+        _url: &v8::inspector::StringView,
+        _line_number: u32,
+        _column_number: u32,
+        _stack_trace: &mut v8::inspector::V8StackTrace,
+    ) {
+        // ログメッセージの出力
+        (self.logger)(message.to_string());
+    }
+}
+
 struct MyStatus {
     audio: Rc<RefCell<Option<v8::Global<v8::Function>>>>,
 }
@@ -192,8 +278,11 @@ fn main() {
     let api = Api::new().add("audio", MyStatus::audio);
     let mut app = JsRuntime::new(data);
     app.reset();
+    app.set_logger(|msg| {
+        println!("Console log: {}", msg);
+    });
     app.add_api("ps88", &api);
-    app.run("let a = 100; ps88.audio((n) => (n + a));");
+    app.run("let a = 100; ps88.audio((n) => (n + a)); console.log('hogehoge');");
 
     {
         let scope = &mut v8::HandleScope::with_context(&mut app.isolate, &app.context);
