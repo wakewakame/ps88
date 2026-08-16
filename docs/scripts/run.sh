44b23c75c1396f623d3173a9c0bccd1e6b1865c5
wakewakame <0214.taiki@gmail.com>
2026-08-16 18:47:51 +0900

    todo の整理

--- docs/scripts/run.sh
+++ docs/scripts/run.sh
@@ -1,8 +1,7 @@
 #!/bin/sh -eu
 cd "$(git rev-parse --show-toplevel)"
-xtask bundle ps88 --release
+cargo nice-plug bundle ps88 --release
 
-# TODO: standalone で動かすときに --input-device / --midi-input / -r が自動でいい感じに設定されるようにしたい
-./target/bundled/ps88.app/Contents/MacOS/ps88 -r 44100 -p 512
-#./target/bundled/ps88.app/Contents/MacOS/ps88 --input-device '外部マイク' -r 44100 -p 512
-#./target/bundled/ps88.app/Contents/MacOS/ps88 --midi-input 'microKEY2-25 Air Bluetooth' -r 44100 -p 512
+./target/bundled/ps88.app/Contents/MacOS/ps88
+#./target/bundled/ps88.app/Contents/MacOS/ps88 --input-device '外部マイク'
+#./target/bundled/ps88.app/Contents/MacOS/ps88 --midi-input 'MPK mini Play mk3'
